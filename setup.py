41c2da74036b46149ad573a148ecec38b4d51f7a
jack-ianson <jackianson2@gmail.com>
2025-08-21 17:57:55 +0100

    added setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="forecast",
+    version="0.1.0",
+    description="Forecast CNN-LSTM",
+    author="Jack I'Anson",
+    packages=find_packages(where="forecast"),
+    package_dir={"": "forecast"},
+)
